d205ebcf9965a58f555edb5544fc73c0234da247
No1ceTea <thaiwomtg@gmail.com>
2025-01-30 17:58:53 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
+      <body>
         {children}
         <Analytics />
       </body>
