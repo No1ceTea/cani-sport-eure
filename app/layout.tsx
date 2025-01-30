00eaffec03f6a778fe11dbbebf5cc5a3ca4c91c8
No1ceTea <thaiwomtg@gmail.com>
2025-01-30 17:59:58 +0100

    fix: inter.className

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body>
+      <body className={inter.className}>
         {children}
         <Analytics />
       </body>
