df5196db5101c2e5afadf4cc97049238e3bd7be1
Nadols <91186769+Nadols@users.noreply.github.com>
2025-03-05 19:54:59 +0100

    save change

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import TitleSection from "./accueil/TitleSection";
 import PresentationSection from "./accueil/PresentationSection";
 import AgendaSection from "./accueil/AgendaSection";
+import Sponsor from "./accueil/Sponsor";
 
 import ScrollLock from "./components/ScrollLock";
 import Grid_5 from "./components/Grid_5";
@@ -17,6 +18,7 @@ export default function HomePage() {
       <TitleSection />
       <PresentationSection />
       <AgendaSection />
+      <Sponsor />
 
       {/* Grille de développement */}
       <Grid_5 />
