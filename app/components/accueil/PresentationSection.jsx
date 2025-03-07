1fde4906ef8e82dfd59562178753563fcae9a8f5
Allan COSTES <allancts.compte@gmail.com>
2025-03-07 10:35:26 +0100

    up css

--- app/components/accueil/PresentationSection.jsx
+++ app/components/accueil/PresentationSection.jsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Presentation() {
   return (
-    <section id="presentation" className="bg-white primary_title_black py-16 px-6 sm:px-16">
+    <section id="presentation" className="bg-white py-16 px-6 sm:px-16">
       {/* Grid principal */}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 container mx-auto">
         
         {/* Colonne gauche - Présentation */}
         <div className="lg:col-span-7">
-          <h1 className="text-4xl font-bold mb-6">Présentation de l&apos;association</h1>
+          <h1 className="text-4xl font-bold mb-6 primary_title_black">Présentation de l&apos;association</h1>
 
-          <p className="mb-8 text-lg leading-relaxed">
+          <p className="mb-8 text-lg leading-relaxed primary_text_black">
             Nous sommes une association passionnée par les sports canins. Notre objectif est de promouvoir le cani-cross,
             le cani-trail, la randonnée avec chiens et d&apos;autres disciplines adaptées aux duos humain-chien.
           </p>
@@ -18,7 +18,7 @@ export default function Presentation() {
           {/* Image mise en avant avec titre */}
           <div className="relative rounded-3xl overflow-hidden mb-8">
             <div className="absolute top-4 left-4 z-10">
-              <h3 className="bg-blue-800 primary_title_black px-6 py-2 rounded-lg text-xl add_border">Nos Activités</h3>
+              <h3 className="bg-blue-800 primary_title px-6 py-2 rounded-lg text-xl add_border">Nos Activités</h3>
             </div>
             <Image
               src="/montagne.jpeg"
@@ -30,7 +30,7 @@ export default function Presentation() {
           </div>
 
           <h2 className="text-2xl font-bold mb-6 primary_title_black">Nos valeurs et objectifs</h2>
-          <p className="text-lg leading-relaxed">
+          <p className="text-lg leading-relaxed primary_text_black text-left">
             Nous organisons des événements sportifs et des rencontres pour partager notre passion avec d&apos;autres
             passionnés. Rejoignez-nous et découvrez une communauté engagée et dynamique !
           </p>
