aa9fa0dbc604141f6a71db7b9e266d2901b52f43
No1ceTea <thaiwomtg@gmail.com>
2025-03-07 11:17:41 +0100

    Réparation des cookies en cours (admin à tester)

--- app/(dashboard)/[userType]/admin/adherents/page.tsx
+++ app/(dashboard)/[userType]/admin/adherents/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import SidebarAdmin from "../../../components/SidebarAdmin";
+import SidebarAdmin from "../../../../components/SidebarAdmin";
 
 interface User {
   id: string;
