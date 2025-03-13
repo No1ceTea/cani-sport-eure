a54b9e886f67fb4a047f166656140727917cc99d
thron91 <97443017+thron91@users.noreply.github.com>
2025-03-13 09:41:24 +0100

    Update page.tsx

--- app/catalogue/page.tsx
+++ app/catalogue/page.tsx
@@ -1,9 +1,10 @@
 import CatalogueSorties from '../components/CatalogueSorties';
+import SidebarAdmin from '../components/SidebarAdmin';
 
 export default function CataloguePage() {
     return (
-      <div className="p-6 bg-white min-h-screen">
-        <h1 className="text-xl font-bold mb-4 text-gray-700"></h1>
+      <div className="flex">
+        <SidebarAdmin />
         <CatalogueSorties />
       </div>
     );
