1961d220e321f604c70159f0f3b45c56154aeef5
No1ceTea <thaiwomtg@gmail.com>
2025-04-08 10:36:12 +0200

    re-add replace

--- app/api/calendar/route.ts
+++ app/api/calendar/route.ts
@@ -11,6 +11,7 @@ console.log("GOOGLE_PRIVATE_KEY (longueur):", process.env.GOOGLE_PRIVATE_KEY?.le
 
 const auth = new google.auth.JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
